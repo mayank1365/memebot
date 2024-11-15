216523dc77c68e95f85133605c6fc2bc367c542a
mayank1365 <mayank1365@gmail.com>
2024-11-15 17:36:43 +0530

    basic interaction

--- bot.py
+++ bot.py
@@ -1,18 +1,18 @@
 import discord
-from dotenv import load_dotenv
-import os
-
-api_key = os.getenv('API_KEY')
-debug = os.getenv('DEBUG')
-
-load_dotenv()
 
 class MyClient(discord.Client):
   async def on_ready(self):
     print('Logged on as {0}!'.format(self.user))
 
+  async def on_message(self, message):
+    if message.author == self.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hello World!")
+
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run(api_key) 
+client.run("") # Add your bot token here
