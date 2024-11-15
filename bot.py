929ec98b093b85b07428758c2adb576d28555c4d
mayank1365 <mayank1365@gmail.com>
2024-11-15 17:38:17 +0530

    added meme interaction

--- bot.py
+++ bot.py
@@ -1,4 +1,11 @@
 import discord
+import requests
+import json
+
+def get_meme():
+  response = requests.get('https://meme-api.com/gimme')
+  json_data = json.loads(response.text)
+  return json_data['url']
 
 class MyClient(discord.Client):
   async def on_ready(self):
@@ -6,10 +13,9 @@ class MyClient(discord.Client):
 
   async def on_message(self, message):
     if message.author == self.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hello World!")
+      return
+    if message.content.startswith('$meme'):
+      await message.channel.send(get_meme())
 
 intents = discord.Intents.default()
 intents.message_content = True
